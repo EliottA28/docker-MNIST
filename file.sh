bf5afcb47848157508ebe674db226812fadd187f
eaubree <aubree.eliott@gmail.com>
2021-11-30 23:12:54 +0100

    build app

---
+++ file.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# Build App
+
+mkdir -p $PWD/app
+cd app
+mkdir -p $PWD/inputs
+mkdir -p $PWD/output
+
+docker run -i --name container1 -v $PWD/inputs:/app/inputs \
+				 -v $PWD/output:/app/output \
+			         eaubree/project:latest
+			         
+			         
+			     
